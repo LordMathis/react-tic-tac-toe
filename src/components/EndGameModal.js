72f905350f61c143a45c6ee80bca4a46ba3b96ae
Matúš Námešný <namesny.matus@gmail.com>
2017-03-22 15:00:42 +0100

    added Fork me on Github ribbon (#3)

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -5,26 +5,25 @@ var Button = ReactBootstrap.Button;
 var PropTypes = React.PropTypes;
 
 function EndGameModal(props) {
-  return (
-    <Modal
-      show={props.show}>
-      <Modal.Header>
-        <Modal.Title id="contained-modal-title">Game over</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-         {props.endGameString}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onPlayAgain}>Play again</Button>
-      </Modal.Footer>
-    </Modal>
-  )
+    return (
+        <Modal show={props.show}>
+            <Modal.Header>
+                <Modal.Title id="contained-modal-title">Game over</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {props.endGameString}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onPlayAgain}>Play again</Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
 
 EndGameModal.propTypes = {
-  show: PropTypes.bool.isRequired,
-  endGameString: PropTypes.string.isRequired,
-  onPlayAgain: PropTypes.func.isRequired
+    show: PropTypes.bool.isRequired,
+    endGameString: PropTypes.string.isRequired,
+    onPlayAgain: PropTypes.func.isRequired
 }
 
 module.exports = EndGameModal;
